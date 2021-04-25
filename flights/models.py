122ad43bf01bba88112766146d372b1a511b0d81
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-25 13:27:01 +0530

    Add city for the class Airport

--- flights/models.py
+++ flights/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Create your models here.
 class Airport(models.Model):
     code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+
 class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
