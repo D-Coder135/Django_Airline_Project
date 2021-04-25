a3228792aebf86af24e2565d3c23ad336ceb10db
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-25 14:25:20 +0530

    Add some fixup!

--- airline/urls.py
+++ airline/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("flight/", include("flights.urls"))
+    path("flights/", include("flights.urls"))
 ]
