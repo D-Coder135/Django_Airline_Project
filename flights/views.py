783409cfd0dbe5683fcecca40e72657765231e93
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-25 13:48:32 +0530

    Return the render() function

--- flights/views.py
+++ flights/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.object.all()
+    })
